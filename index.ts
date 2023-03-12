02011a080e691e37c98275e282c8283b3e9d5b22
Michal <michal.zelechowski@o2.pl>
2023-03-12 14:12:55 +0100

    Adding validation errors

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError} from "./utils/errors";
+import {userRouter} from "./routers/user.router";
 
 const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+
+app.use('/', userRouter);
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Aplikacja działą pod url: http://localhost:3001')
