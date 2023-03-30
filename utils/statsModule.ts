487d06cf46d22461f017bfa5410aeb231fa15d20
Michal <michal.zelechowski@o2.pl>
2023-03-30 17:59:14 +0200

    Adding statsModule.

---
+++ utils/statsModule.ts
@@ -0,0 +1,9 @@
+import {SingleUserEntity} from "../types/user/user-login-data";
+
+export const getPercentageValue = async (firstParam: number, secondParam: number = 0): Promise<SingleUserEntity| number> => {
+    return (Math.floor(firstParam / 10) + secondParam)
+};
+
+export const getHP = async (stamina: number, level:number): Promise<number> => {
+    return stamina * 5 + level * 15;
+}
